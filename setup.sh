d019a58a9d3d929562b393e483326a36555e1e1f
jonathon deason <jon23d@gmail.com>
2026-03-06 22:53:13 +0000

    add vscode to setup

--- setup.sh
+++ setup.sh
@@ -10,3 +10,9 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 24
+
+# vs code
+wget -O vscode_cli.tar.gz "https://update.code.visualstudio.com/latest/cli-linux-x64/stable"
+gunzip vscode_cli.tar.gz
+sudo mv code /usr/local/bin/
+rm vscode_cli.tar.gz
