3293f18bc53f6296365cffe85f37c1d47c469d2e
jonathon deason <jon23d@gmail.com>
2026-03-06 22:28:47 +0000

    add node setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+
+# opencode
 curl -fsSL https://opencode.ai/install | bash
+
+# uv
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 24
