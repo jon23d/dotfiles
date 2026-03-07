1ac1b5a4e91b0daf3e614d36caad5b57d6fe235d
jonathon deason <jon23d@gmail.com>
2026-03-07 13:35:35 +0000

    add opencode alias

--- setup.sh
+++ setup.sh
@@ -12,7 +12,16 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.4/install.sh | bash
 nvm install 24
 
 # vs code
+mkdir tmp
+cd tmp
 wget -O vscode_cli.tar.gz "https://update.code.visualstudio.com/latest/cli-linux-x64/stable"
 gunzip vscode_cli.tar.gz
+tar -xvf vscode_cli.tar
 sudo mv code /usr/local/bin/
 rm vscode_cli.tar.gz
+cd ..
+rm -fR tmp
+
+
+echo "alias vscode=\"code serve-web --host 0.0.0.0 --port 8080 --without-connection-token\"" >> ~/.bashrc
+echo "alias openserve=\"opencode web --hostname 0.0.0.0\"" >> ~/.bashrc
