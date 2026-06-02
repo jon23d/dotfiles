d71924c469685f77e0242b76e45f9af41d794f8f
Jonathon Deason <jon23d@gmail.com>
2026-06-02 13:11:41 +0000

    add path to lualined

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -179,7 +179,17 @@ require("lazy").setup({
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-      require("lualine").setup({ options = { theme = "tokyonight" } })
+        require("lualine").setup({
+          options = { theme = "tokyonight" },
+          sections = {
+            lualine_c = {
+              {
+                "filename",
+                path = 3,  -- 3 = absolute path with ~
+              },
+            },
+          },
+        })
     end,
   },
 
