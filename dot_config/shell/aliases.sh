d340497926545dd2de27a6bc09d84919761c60d1
Jonathon Deason <jon23d@gmail.com>
2026-08-15 11:37:28 -0400

    alias openclaude

--- dot_config/shell/aliases.sh
+++ dot_config/shell/aliases.sh
@@ -4,5 +4,6 @@ alias ll='ls -lA'
 alias k='kubectl'
 alias co='git checkout'
 alias notes='NVIM_APPNAME=nvim-notes nvim'
+alias openclaude='claude --system-prompt-file ~/.config/opencode/agents/build.md' 
 
 export EDITOR='nvim'
